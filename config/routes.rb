7500ad066b93c2471547e27bf3c922aa74e64ee9
John Grubb <therealjohnnygrubb@gmail.com>
2013-08-23 15:55:39 -0400

    Registration and zurbification

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Shock::Application.routes.draw do
+  devise_for :users, path_names: { sign_in: "login", sign_out: "logout", registration: "register"  }
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
