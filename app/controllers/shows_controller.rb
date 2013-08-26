8350a8c1cce35d84f82f074e2fb04f529804613a
John Grubb <therealjohnnygrubb@gmail.com>
2013-08-26 14:16:16 -0400

    Getting there w forms and stuff

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -10,7 +10,7 @@ class ShowsController < ApplicationController
   end
 
   def create
-    @show = Show.new params[:show]
+    @show = Show.new show_params
     if @show.save
       flash[:notice] = "#{@show.date} was successfully created."
       redirect_to root_path
@@ -21,14 +21,23 @@ class ShowsController < ApplicationController
   end
 
   def edit
-
+    @show = Show.find params[:id]
   end
 
   def update
-
+    @show = Show.find params[:id]
+    if @show.update_attributes show_params
+      redirect_to root_path
+    end
   end
 
   def delete
 
+  end
+
+  private
+
+  def show_params
+    params.require(:show).permit(:date, :venue, :location, :info, :link)
   end
 end
