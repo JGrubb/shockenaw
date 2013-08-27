35a236c2458022fcf46aeed67b111ccdf387acf2
John Grubb <therealjohnnygrubb@gmail.com>
2013-08-27 17:49:12 +0000

    Site soft launch

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,6 +66,7 @@ Shock::Application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'http://shockenaw.com' }
+  config.action_mailer.delivery_method = :smtp
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
