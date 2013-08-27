9dbdaebf5bee039ccf2b28debf8de2034d19d164
John Grubb <therealjohnnygrubb@gmail.com>
2013-08-27 17:49:12 +0000

    Site soft launch

--- app/models/user.rb
+++ app/models/user.rb
@@ -23,9 +23,9 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
+  # :token_authenticatable, :confirmable, :registerable,
   # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable, :confirmable,
+  devise :database_authenticatable, :confirmable,
          :recoverable, :rememberable, :trackable, :validatable
   
 end
