a04d4d54982a9e9a1b03b86f8b340154294479ea
John Grubb <grubb@platform.sh>
2017-08-24 14:21:43 -0400

    flipping to port 465 for email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,9 @@ Shock::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   config.action_mailer.default_url_options = { host: 'http://shockenaw.com' }
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+      :port = 465
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation can not be found).
